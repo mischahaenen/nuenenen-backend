68d52640fd62828c6d19da74e91c421bfc57b5ea
Mischa Haenen <mischahaenen@gmail.com>
2023-09-10 12:32:55 +0200

    feat(senderlist): add senderlist controller

---
+++ src/api/contact-distribution-list/controllers/contact-distribution-list.ts
@@ -0,0 +1,7 @@
+/**
+ * contact-distribution-list controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::contact-distribution-list.contact-distribution-list');
